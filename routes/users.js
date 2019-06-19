2c92690548dd43837432ea29a4bf5032a6942966
Christoph Machuletz <machuletz.christoph@gmail.com>
2019-06-19 21:36:02 +0200

    routing

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+//DB connection for plesk
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'alexa_db_user',
+    password : '10iw?i3I',
+    database : 'cm_alexa'
+});
+
+//Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySQL Connected');
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	connection.query('SELECT * from bewohner', function (error, results, fields) {
+	db.query('SELECT * from bewohner', function (error, results, fields) {
 		if (error) throw error;
 		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
 	});
