aa995e76ee7750aa3865fc4ff209b9f2a000fb7e
Christoph Machuletz <machuletz.christoph@gmail.com>
2019-06-19 22:22:58 +0200

    routing

--- app.js
+++ app.js
@@ -27,21 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bewohner', require('./routes/bewohner'));
 app.use('/pfleger', require('./routes/pfleger'));
 
-//DB connection for plesk
-const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'alexa_db_user',
-  password : '10iw?i3I',
-  database : 'cm_alexa'
-});
 
-//Connect
-db.connect((err) => {
-  if(err){
-      throw err;
-  }
-  console.log('MySQL Connected');
-});
 
 
 // catch 404 and forward to error handler
