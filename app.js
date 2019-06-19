5cb3ff9b9ed10771d64b7a4a897fcbe007f0f9bd
Christoph Machuletz <machuletz.christoph@gmail.com>
2019-06-19 22:26:38 +0200

    routing

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/bewohner', require('./routes/bewohner'));
-app.use('/pfleger', require('./routes/pfleger'));
+//app.use('/pfleger', require('./routes/pfleger'));
 
 //DB connection for plesk
 const db = mysql.createConnection({
@@ -41,6 +41,22 @@ db.connect((err) => {
       throw err;
   }
   console.log('MySQL Connected');
+});
+
+//Select all records from pfleger
+app.get('/', function(req, res, next) {
+	db.query('SELECT * FROM pfleger', function (err, results) {
+        if (err) throw err;
+        res.type('application/json').send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
+
+//Select a single record from pfleger
+app.get('/:id', function(req, res, next) {
+	db.query(`SELECT * FROM pfleger WHERE pfleger_id = ${req.params.id}`, function (err, results) {
+		if (err) throw err;
+		res.type('application/json').send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
 });
 
 
