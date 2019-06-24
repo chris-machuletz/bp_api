8e2d285edb0a11f87d5885e592529c468190a381
Christoph Machuletz <machuletz.christoph@gmail.com>
2019-06-24 21:56:45 +0200

    updated bewohner post request at /

--- routes/bewohner.js
+++ routes/bewohner.js
@@ -19,7 +19,8 @@ router.get('/:id', function(req, res, next) {
 });
 
 // Add new record to bewohner
-router.post('/test', function(req, res) {
+router.post('/', function(req, res) {
+	
 	// const newBewohner = {
 	// 	nachname: req.body.nachname,
 	// 	vorname: req.body.vorname,
@@ -28,15 +29,24 @@ router.post('/test', function(req, res) {
 	// 	geburtsdatum: req.body.geburtsdatum,
 	// 	geschlecht: req.body.geschlecht
 	// }
-	// db.query(`INSERT INTO bewohner(nachname, vorname, zimmernummer, pflegegrad, geburtsdatum, geschlecht) VALUES('test', 'test', 101, 1, STR_TO_DATE('1-01-2012', '%d-%m-%Y'), 'm')`, function (err, results) {
-	// 	if (err) {
-	// 		throw err
-	// 	} else {
-	// 		res.type('application/json').send(JSON.stringify({"status": 200, "action": "post@bewohner", "error": null, "response": "Bewohner successfully inserted"}));
-	// 	}
-	// });
+	db.query(`INSERT INTO bewohner (nachname, vorname, zimmernummer, pflegegrad, geburtsdatum, geschlecht) VALUES ('test', 'test', 101, 1, STR_TO_DATE('1-01-2012', '%d-%m-%Y'), 'm')`, function (err, results) {
+		if (err) {
+			throw err
+		} else {
+			res.type('application/json').send(JSON.stringify({"status": 200, "action": "post@bewohner", "error": null, "response": "Bewohner successfully inserted"}));
+		}
+	});
 
-	res.type('application/json').send(JSON.stringify({"status": 200, "action": "post@bewohner/test", "error": null}));
+	// res.type('application/json').send(JSON.stringify({"status": 200, "action": "post@bewohner/test", "error": null}));
 });
+
+// router.post("/api/data", (request, response) => {
+// 	const data = request.body;
+// 	const timestamp = Date.now();
+// 	data.timestamp = timestamp;
+// 	database.insert(data);
+// 	console.log("Neuer HighScore eingetragen");
+// 	response.end();
+//   });
 
 module.exports = router;
