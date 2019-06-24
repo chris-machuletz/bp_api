ac1c5d6cad6b6f4a74ec559b26e1a4af833c7017
chris-machuletz <machuletz.christoph@gmail.com>
2019-06-24 13:23:07 +0200

    add json limit to app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ limit: '1mb' }));
 
 
 app.use('/bewohner', require('./routes/bewohner'));
