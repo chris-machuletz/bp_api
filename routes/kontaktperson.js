225a5f65d68d1694d1a3de08e04a29f2ad0c2040
Christoph Machuletz <machuletz.christoph@gmail.com>
2019-06-25 13:50:34 +0200

    modified kontaktperson mysql request and json output

--- routes/kontaktperson.js
+++ routes/kontaktperson.js
@@ -4,9 +4,23 @@ const db = require('../dbconn');
 
 // Select all records from kontaktperson
 router.get('/', function(req, res, next) {
-	db.query('SELECT * FROM kontaktperson', function (err, results) {
+	db.query(`SELECT kontaktperson.kontaktperson_id, kontaktperson.nachname, kontaktperson.vorname, kp_bezeichnung.name, CONCAT(bewohner.vorname, ' ', bewohner.nachname) as bewohner, kontaktperson.telefon FROM kontaktperson\
+    INNER JOIN kp_bezeichnung ON kontaktperson.kp_bezeichnung_id = kp_bezeichnung.kp_bezeichnung_id\
+    INNER JOIN bewohner ON kontaktperson.bewohner_id = bewohner.bewohner_id`, function (err, results) {
         if (err) throw err;
         res.type('application/json').send(JSON.stringify({"status": 200, "action": "get@kontaktperson/", "error": null, "response": results}));
+	});
+});
+
+// Select record from kontaktperson where :id = bewohner_id
+router.get('/:id', function(req, res, next) {
+	db.query(`SELECT kontaktperson.kontaktperson_id, kontaktperson.nachname, kontaktperson.vorname, kp_bezeichnung.name, kontaktperson.telefon FROM kontaktperson\
+    INNER JOIN kp_bezeichnung ON kontaktperson.kp_bezeichnung_id=kp_bezeichnung.kp_bezeichnung_id\
+    INNER JOIN bewohner ON kontaktperson.bewohner_id=bewohner.bewohner_id\
+    WHERE kontaktperson.bewohner_id=${req.params.id}`
+    , function (err, results) {
+        if (err) throw err;
+        res.type('application/json').send(JSON.stringify({"status": 200, "action": "get@kontaktperson/:id", "error": null, "response": results}));
 	});
 });
 
