1eafb20091bc5f853b657917e290429dda350579
chris-machuletz <machuletz.christoph@gmail.com>
2019-07-01 13:54:15 +0200

    updated bewohner:zimmernummer route

--- routes/bewohner.js
+++ routes/bewohner.js
@@ -19,8 +19,8 @@ router.get('/:id', function(req, res, next) {
 });
 
 // Select a single record from bewohner where id=zimmernummer
-router.get('/:zimmernummer', function(req, res, next) {
-	db.query(`SELECT *, DATE_FORMAT(geburtsdatum, "%d.%m.%Y") as geburtsdatum FROM bewohner WHERE zimmernummer = ${req.params.zimmernummer}`, function (err, results) {
+router.get('/zimmernummer/:id', function(req, res, next) {
+	db.query(`SELECT *, DATE_FORMAT(geburtsdatum, "%d.%m.%Y") as geburtsdatum FROM bewohner WHERE zimmernummer = ${req.params.id}`, function (err, results) {
 		if (err) throw err;
 		res.type('application/json').send(JSON.stringify({"status": 200, "action": "get@bewohner/:zimmernummer", "error": null, "response": results}));
 	});
